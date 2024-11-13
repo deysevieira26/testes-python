42300a991df89bd2e18ef1b0325c6ffca83b98de
deysevieira26 <deyservieira@gmail.com>
2024-11-13 16:06:35 -0300

    Arquivos iniciais

---
+++ CalculadoraIMC.py
@@ -0,0 +1,11 @@
+# Criar a classe 
+# chamada "CalculadoraIMC"
+class CalculadoraIMC:
+    def calcular_imc (peso,altura):
+        if altura <= 0:
+
+
+imc < 19 : magreza
+imc >= 19 e imc < 24 : normal
+imc >= 24 e imc < 29 : sobrepeso
+imc >= 29 : obesidade
